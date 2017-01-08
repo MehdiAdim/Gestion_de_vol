388b352f739ef6c57e2847da78bc139cda0f4512
Barhdadi <xakea.mffl@gmail.com>
2017-01-08 05:12:54 +0000

    Première modification

--- Compagnieaerienne.java
+++ Compagnieaerienne.java
@@ -1,188 +1,199 @@
-/***********************************************************************
- * Module:  Compagnieaerienne.java
- * Author:  imannaa
- * Purpose: Defines the Class Compagnieaerienne
- ***********************************************************************/
-
+package gestion_de_vols;
 import java.util.*;
+public class Compagnieaerienne 
+{
+	public static int nbrca;
+	private int idComp;
+	private String nomCompAirien;
+	public java.util.Collection<Volcomplet> volcomplet;
+	public java.util.Collection<Vol> vol;
+	public java.util.Collection<Client> client;
+		   
+	   
+	public Compagnieaerienne( String nomCompAirien) 
+	{
+		this.idComp = nbrca;
+		this.nomCompAirien = nomCompAirien;
+		nbrca++;
+	}
+	
+	
 
-/** @pdOid 6d3a12c7-0a90-4b86-bc30-10af7c61474c */
-public class Compagnieaerienne {
-   /** @pdOid dde94089-0e1b-44fa-9210-903823007949 */
-   private int idComp;
-   /** @pdOid 22e25045-6ac7-49a0-b7ef-a34185711abd */
-   private String nomCompAirien;
-   
-   /** @pdRoleInfo migr=no name=Volcomplet assc=proposevolcomplet coll=java.util.Collection impl=java.util.HashSet mult=0..* */
-   public java.util.Collection<Volcomplet> volcomplet;
-   /** @pdRoleInfo migr=no name=Vol assc=proposevol coll=java.util.Collection impl=java.util.HashSet mult=0..* */
-   public java.util.Collection<Vol> vol;
-   /** @pdRoleInfo migr=no name=Client assc=association2 coll=java.util.Collection impl=java.util.HashSet mult=0..* */
-   public java.util.Collection<Client> client;
-   
-   /** @pdOid 89e28564-7cf2-48b7-abf9-3cc76b0316fa */
-   public Compagnieaerienne() {
-      // TODO: implement
-   }
-   
-   
-   /** @pdGenerated default getter */
-   public java.util.Collection<Volcomplet> getVolcomplet() {
-      if (volcomplet == null)
-         volcomplet = new java.util.HashSet<Volcomplet>();
-      return volcomplet;
-   }
-   
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorVolcomplet() {
-      if (volcomplet == null)
-         volcomplet = new java.util.HashSet<Volcomplet>();
-      return volcomplet.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newVolcomplet */
-   public void setVolcomplet(java.util.Collection<Volcomplet> newVolcomplet) {
-      removeAllVolcomplet();
-      for (java.util.Iterator iter = newVolcomplet.iterator(); iter.hasNext();)
-         addVolcomplet((Volcomplet)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newVolcomplet */
-   public void addVolcomplet(Volcomplet newVolcomplet) {
-      if (newVolcomplet == null)
-         return;
-      if (this.volcomplet == null)
-         this.volcomplet = new java.util.HashSet<Volcomplet>();
-      if (!this.volcomplet.contains(newVolcomplet))
-      {
-         this.volcomplet.add(newVolcomplet);
-         newVolcomplet.setCompagnieaerienne(this);      
-      }
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldVolcomplet */
-   public void removeVolcomplet(Volcomplet oldVolcomplet) {
-      if (oldVolcomplet == null)
-         return;
-      if (this.volcomplet != null)
-         if (this.volcomplet.contains(oldVolcomplet))
-         {
-            this.volcomplet.remove(oldVolcomplet);
-            oldVolcomplet.setCompagnieaerienne((Compagnieaerienne)null);
-         }
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllVolcomplet() {
-      if (volcomplet != null)
-      {
-         Volcomplet oldVolcomplet;
-         for (java.util.Iterator iter = getIteratorVolcomplet(); iter.hasNext();)
-         {
-            oldVolcomplet = (Volcomplet)iter.next();
-            iter.remove();
-            oldVolcomplet.setCompagnieaerienne((Compagnieaerienne)null);
-         }
-      }
-   }
-   /** @pdGenerated default getter */
-   public java.util.Collection<Vol> getVol() {
-      if (vol == null)
-         vol = new java.util.HashSet<Vol>();
-      return vol;
-   }
-   
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorVol() {
-      if (vol == null)
-         vol = new java.util.HashSet<Vol>();
-      return vol.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newVol */
-   public void setVol(java.util.Collection<Vol> newVol) {
-      removeAllVol();
-      for (java.util.Iterator iter = newVol.iterator(); iter.hasNext();)
-         addVol((Vol)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newVol */
-   public void addVol(Vol newVol) {
-      if (newVol == null)
-         return;
-      if (this.vol == null)
-         this.vol = new java.util.HashSet<Vol>();
-      if (!this.vol.contains(newVol))
-         this.vol.add(newVol);
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldVol */
-   public void removeVol(Vol oldVol) {
-      if (oldVol == null)
-         return;
-      if (this.vol != null)
-         if (this.vol.contains(oldVol))
-            this.vol.remove(oldVol);
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllVol() {
-      if (vol != null)
-         vol.clear();
-   }
-   /** @pdGenerated default getter */
-   public java.util.Collection<Client> getClient() {
-      if (client == null)
-         client = new java.util.HashSet<Client>();
-      return client;
-   }
-   
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorClient() {
-      if (client == null)
-         client = new java.util.HashSet<Client>();
-      return client.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newClient */
-   public void setClient(java.util.Collection<Client> newClient) {
-      removeAllClient();
-      for (java.util.Iterator iter = newClient.iterator(); iter.hasNext();)
-         addClient((Client)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newClient */
-   public void addClient(Client newClient) {
-      if (newClient == null)
-         return;
-      if (this.client == null)
-         this.client = new java.util.HashSet<Client>();
-      if (!this.client.contains(newClient))
-         this.client.add(newClient);
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldClient */
-   public void removeClient(Client oldClient) {
-      if (oldClient == null)
-         return;
-      if (this.client != null)
-         if (this.client.contains(oldClient))
-            this.client.remove(oldClient);
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllClient() {
-      if (client != null)
-         client.clear();
-   }
+	public int getIdComp() 
+	{
+		return idComp;
+	}
 
+
+
+	public void setIdComp(int idComp) 
+	{
+		this.idComp = idComp;
+	}
+
+
+
+	public String getNomCompAirien() 
+	{
+		return nomCompAirien;
+	}
+
+
+
+	public void setNomCompAirien(String nomCompAirien) 
+	{
+		this.nomCompAirien = nomCompAirien;
+	}
+
+
+
+	public Collection<Volcomplet> getVolcomplet() 
+	{
+	   if (volcomplet == null)
+	      volcomplet = new HashSet<Volcomplet>();
+	   return volcomplet;
+	}
+	   
+	public Iterator getIteratorVolcomplet() 
+	{
+	   if (volcomplet == null)
+	      volcomplet = new HashSet<Volcomplet>();
+	   return volcomplet.iterator();
+	}
+	   
+	public void setVolcomplet(Collection<Volcomplet> newVolcomplet) 
+	{
+	   removeAllVolcomplet();
+	   for (Iterator iter = newVolcomplet.iterator(); iter.hasNext();)
+	      addVolcomplet((Volcomplet)iter.next());
+	}
+	   
+	public void addVolcomplet(Volcomplet newVolcomplet) 
+	{
+		if (newVolcomplet == null)
+			return;
+		if (this.volcomplet == null)
+			this.volcomplet = new HashSet<Volcomplet>();
+	    if (!this.volcomplet.contains(newVolcomplet))
+	    {
+	       this.volcomplet.add(newVolcomplet);
+	       newVolcomplet.setCompagnieaerienne(this);      
+	    }
+	}
+	   
+	public void removeVolcomplet(Volcomplet oldVolcomplet) 
+	{
+		if (oldVolcomplet == null)
+	    return;
+	    if (this.volcomplet != null)
+	      if (this.volcomplet.contains(oldVolcomplet))
+	      {
+	         this.volcomplet.remove(oldVolcomplet);
+	         oldVolcomplet.setCompagnieaerienne((Compagnieaerienne)null);
+	      }
+	}
+	   
+	public void removeAllVolcomplet() 
+	{
+	   if (volcomplet != null)
+	   {
+	      Volcomplet oldVolcomplet;
+	      for (Iterator iter = getIteratorVolcomplet(); iter.hasNext();)
+	      {
+	         oldVolcomplet = (Volcomplet)iter.next();
+	         iter.remove();
+	         oldVolcomplet.setCompagnieaerienne((Compagnieaerienne)null);
+	      }
+	   }
+	}
+	public Collection<Vol> getVol() 
+	{
+	   if (vol == null)
+	      vol = new HashSet<Vol>();
+	   return vol;
+	}
+	
+	public Iterator getIteratorVol() 
+	{
+	   if (vol == null)
+	      vol = new HashSet<Vol>();
+	   return vol.iterator();
+	}
+	
+	public void setVol(Collection<Vol> newVol) 
+	{
+	   removeAllVol();
+	   for (Iterator iter = newVol.iterator(); iter.hasNext();)
+	      addVol((Vol)iter.next());
+	}
+	
+	public void addVol(Vol newVol) 
+	{
+	   if (newVol == null)
+	      return;
+	   if (this.vol == null)
+	      this.vol = new HashSet<Vol>();
+	   if (!this.vol.contains(newVol))
+	      this.vol.add(newVol);
+	}
+	
+	public void removeVol(Vol oldVol) 
+	{
+	   if (oldVol == null)
+	      return;
+	   if (this.vol != null)
+	      if (this.vol.contains(oldVol))
+	         this.vol.remove(oldVol);
+	}
+	
+	public void removeAllVol() 
+	{
+	   if (vol != null)
+	     vol.clear();
+	}
+	public Collection<Client> getClient() 
+	{
+	   if (client == null)
+	      client = new HashSet<Client>();
+	   return client;
+	}
+	public Iterator getIteratorClient() 
+	{
+	    if (client == null)
+	      client = new HashSet<Client>();
+	   return client.iterator();
+	}
+	
+	public void setClient(Collection<Client> newClient) 
+	{
+	   removeAllClient();
+	   for (Iterator iter = newClient.iterator(); iter.hasNext();)
+	      addClient((Client)iter.next());
+	}
+	
+	public void addClient(Client newClient) 
+	{
+	   if (newClient == null)
+	      return;
+	   if (this.client == null)
+	      this.client = new HashSet<Client>();
+	   if (!this.client.contains(newClient))
+	      this.client.add(newClient);
+	}
+	
+	public void removeClient(Client oldClient) 
+	{
+	   if (oldClient == null)
+	      return;
+	   if (this.client != null)
+	      if (this.client.contains(oldClient))
+	         this.client.remove(oldClient);
+	}
+	
+	public void removeAllClient() 
+	{
+	   if (client != null)
+	      client.clear();
+	}	
 }
+
--- Passager.java
+++ Passager.java
@@ -1,36 +1,51 @@
-/***********************************************************************
- * Module:  Passager.java
- * Author:  simo
- * Purpose: Defines the Class Passager
- ***********************************************************************/
+package gestion_de_vols;
 
-import java.util.*;
+public class Passager 
+{
+	private String nomPassager;
+	private String prenomPassager;
+	   
+	public Reservation[] reservation;
+	   
+	public Passager(String nomPassager,String prenomPassager) 
+	{
+		this.nomPassager= nomPassager;
+		this.prenomPassager=prenomPassager;
+	}
+	
+	  
+	public String getNomPassager() 
+	{
+		return nomPassager;
+	}
 
-/** @pdOid 64dce591-5e8b-4a14-80c6-95c6cfd3fcc6 */
-public class Passager {
-   /** @pdOid e531adc4-ab61-4685-9923-0cf108499abe */
-   private String nomPassager;
-   /** @pdOid 6acc9342-38ec-4c13-80be-8efe1f414863 */
-   private String prenomPassager;
-   
-   /** @pdRoleInfo migr=no name=Reservation assc=concerne mult=0..* side=A */
-   public Reservation[] reservation;
-   
-   /** @pdOid e6e9f8bf-d66c-487b-b0c4-222b4f37272c */
-   public Passager() {
-      // TODO: implement
-   }
-   
-   
-   /** @pdGenerated default getter */
-   public Reservation[] getReservation() {
-      return reservation;
-   }
-   
-   /** @pdGenerated default setter
-     * @param newReservation */
-   public void setReservation(Reservation[] newReservation) {
-      this.reservation = newReservation;
-   }
 
+	public void setNomPassager(String nomPassager) 
+	{
+		this.nomPassager = nomPassager;
+	}
+
+
+	public String getPrenomPassager() 
+	{
+		return prenomPassager;
+	}
+
+
+	public void setPrenomPassager(String prenomPassager) 
+	{
+		this.prenomPassager = prenomPassager;
+	}
+
+
+	public Reservation[] getReservation() 
+	{
+		return reservation;
+	}
+	   
+	public void setReservation(Reservation[] newReservation) 
+	{
+		this.reservation = newReservation;
+    }
 }
+
